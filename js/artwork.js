9df8d12461eb4fb08f3e34a707dcda00190bc2f6
Denise-Cramer <cramdeni@gmail.com>
2026-07-18 12:24:08 +0200

    complete the dynamic artwork details page

--- js/artwork.js
+++ js/artwork.js
@@ -13,6 +13,37 @@ async function testSingleArtwork() {
         const artwork = await getArtwork(artworkId);
 
         console.log("Selected artwork:", artwork);
+
+        document.getElementById("artwork-title").textContent =
+            artwork.title;
+        
+        document.getElementById("artwork-artist").textContent =
+                artwork.artist;
+
+        document.getElementById("artwork-year").textContent =
+            artwork.year
+
+        document.getElementById("artwork-medium").textContent =
+            artwork.medium
+
+        document.getElementById("artwork-location").textContent =
+            artwork.location
+
+        document.getElementById("artwork-description").textContent =
+            artwork.description
+
+        const artworkImage =
+            document.getElementById("artwork-image");
+
+        artworkImage.src =
+            artwork.image?.url ||
+            "https://placehold.co/800x600";
+
+        artworkImage.alt =
+            artwork.image?.alt ||
+            artwork.title ||
+            "Artwork image";
+            
     } catch (error) {
         console.error("Could not load selected artwork:", error);
     }
